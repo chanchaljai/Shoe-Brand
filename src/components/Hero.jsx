6662303d31b1113568e142225e1980b15468c839
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-23 11:39:45 +0530

    nav + hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import pic from "../assets/hero-image.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <div className='md:flex justify-end items-center gap-40 max-w-7xl mx-auto px-4 md:px-12 mt-12'>
+        <div>
+          <h1 className='text-5xl md:text-8xl font-bold'>YOUR FEET </h1>
+          <h1 className='text-5xl md:text-8xl font-bold'>DESERVE</h1>
+          <h1 className='text-5xl md:text-8xl font-bold'>THE BEST</h1>
+          <p className='mt-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia totam dolorem ab ad fuga at nihil quibusdam incidunt a eos!</p>
+        </div>
+        <div>
+          <img src={pic} alt="pic" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/brand_logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div className="flex justify-between items-center max-w-7xl mx-auto px-4 md:px-12 py-4">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <ul className="flex gap-8 hidden md:flex text-lg">
+          <li>
+            <Link to="/#">Menu</Link>
+          </li>
+          <li>
+            <Link to="/#">Location</Link>
+          </li>
+          <li>
+            <Link to="/#">About</Link>
+          </li>
+          <li>
+            <Link to="/#">Contact</Link>
+          </li>
+        </ul>
+        <button className="bg-red-700 text-white px-2 py-1 text-lg font-semibold rounded hover:bg-red-600 hover:cursor-pointer">Login</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
